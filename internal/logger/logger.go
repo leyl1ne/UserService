afe3d1a88475e605a0aec3fceb74dfb7ef4e399b
leyl1ne <d-khodin@inbox.ru>
2026-04-24 19:41:50 +0300

    feat(logging): add logger interface

---
+++ internal/logger/logger.go
@@ -0,0 +1,24 @@
+package logger
+
+type Logger interface {
+	With(fields ...Field) Logger
+
+	Info(msg string, fields ...Field)
+	Warn(msg string, fields ...Field)
+	Debug(msg string, fields ...Field)
+	Error(msg string, fields ...Field)
+	Fatal(msg string, fields ...Field)
+	Panic(msg string, fields ...Field)
+}
+
+type Field struct {
+	Key   string
+	Value interface{}
+}
+
+func Err(err error) Field {
+	return Field{
+		Key:   "error",
+		Value: err.Error(),
+	}
+}
