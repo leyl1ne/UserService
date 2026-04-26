677acad4aacc8d1cce1dbe87844b70666fef976c
leyl1ne <d-khodin@inbox.ru>
2026-04-26 13:37:49 +0300

    chore: add transaction wrapper

---
+++ pkg/transaction/transaction.go
@@ -0,0 +1,52 @@
+package transaction
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type ctxKey struct{}
+
+type Transaction struct {
+	Tx pgx.Tx
+}
+
+func Extract(ctx context.Context) *Transaction {
+	val := ctx.Value(ctxKey{})
+	if val == nil {
+		return nil
+	}
+
+	tx, ok := val.(*Transaction)
+	if !ok {
+		return nil
+	}
+
+	return tx
+}
+
+func Wrap(ctx context.Context, pool *pgxpool.Pool, fn func(ctx context.Context) error) error {
+	tx, err := pool.Begin(ctx)
+	if err != nil {
+		return fmt.Errorf("pool.Begin: %w", err)
+	}
+
+	defer func() {
+		_ = tx.Rollback(ctx)
+	}()
+
+	ctx = context.WithValue(ctx, ctxKey{}, &Transaction{Tx: tx})
+
+	if err := fn(ctx); err != nil {
+		return fmt.Errorf("fn: %w", err)
+	}
+
+	if err := tx.Commit(ctx); err != nil {
+		return fmt.Errorf("tx.Commit: %w", err)
+	}
+
+	return nil
+}
