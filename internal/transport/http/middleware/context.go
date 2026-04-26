961b10183c87d4188d10e362e605f483bd54d430
leyl1ne <d-khodin@inbox.ru>
2026-04-26 19:16:17 +0300

    feat(middleware): add context function

---
+++ internal/transport/http/middleware/context.go
@@ -0,0 +1,51 @@
+package middleware
+
+import "github.com/gin-gonic/gin"
+
+type contextKey string
+
+type UserContext struct {
+	UserID    string
+	UserRole  string
+	CompanyID string
+}
+
+const (
+	UserContextKey      contextKey = "user"
+	RequestIDContextKey contextKey = "request_id"
+)
+
+func GetUser(c *gin.Context) (UserContext, bool) {
+	userContext, exists := c.Get(UserContextKey)
+	if !exists {
+		return UserContext{}, false
+	}
+	user, ok := userContext.(UserContext)
+	if !ok {
+		return UserContext{}, false
+	}
+
+	return user, true
+
+}
+
+func SetUser(c *gin.Context, user UserContext) {
+	c.Set(UserContextKey, user)
+}
+
+func SetRequestID(c *gin.Context, requestID string) {
+	c.Set(RequestIDContextKey, requestID)
+}
+
+func GetRequestID(c *gin.Context) string {
+	val, exists := c.Get(RequestIDContextKey)
+	if !exists {
+		return ""
+	}
+
+	id, ok := val.(string)
+	if !ok {
+		return ""
+	}
+	return id
+}
