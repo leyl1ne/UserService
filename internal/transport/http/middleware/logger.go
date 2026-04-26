9547e166744168d6e209e11dcd93a9fd2add886d
leyl1ne <d-khodin@inbox.ru>
2026-04-26 19:15:16 +0300

    feat(middleware): add logger middleware

---
+++ internal/transport/http/middleware/logger.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/leyl1ne/UserService/internal/logger"
+)
+
+func LoggerMiddleware(log logger.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		log = log.With(
+			logger.Field{Key: "component", Value: "middleware/logger"},
+		)
+
+		reqID := uuid.NewString()
+		SetRequestID(c, reqID)
+
+		reqLogger := log.With(
+			logger.Field{Key: "request_id", Value: reqID},
+			logger.Field{Key: "method", Value: c.Request.Method},
+			logger.Field{Key: "path", Value: c.FullPath()},
+		)
+
+		start := time.Now()
+
+		c.Next()
+
+		defer func() {
+			reqLogger.Info("request completed",
+				logger.Field{Key: "status", Value: c.Writer.Status()},
+				logger.Field{Key: "latency", Value: time.Since(start).String()},
+			)
+		}()
+
+	}
+}
