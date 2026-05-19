0659c1adb476d8b7fc1a9b4bf28300877ab42cac
leyl1ne <d-khodin@inbox.ru>
2026-05-19 14:07:07 +0300

    feat(repo): add NewRepository func and Repository struct

---
+++ internal/repository/postgres/repository.go
@@ -0,0 +1,11 @@
+package postgres
+
+import "github.com/jackc/pgx/v5/pgxpool"
+
+type Repository struct {
+	pool *pgxpool.Pool
+}
+
+func NewRepository(pool *pgxpool.Pool) *Repository {
+	return &Repository{pool: pool}
+}
