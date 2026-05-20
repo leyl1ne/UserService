b1cd4849b57e546f90a839db267995a95f79e959
leyl1ne <d-khodin@inbox.ru>
2026-05-20 11:07:51 +0300

    feat(user): add custom error

--- internal/model/user/errors.go
+++ internal/model/user/errors.go
@@ -3,7 +3,8 @@ package user
 import "errors"
 
 var (
-	ErrUserNotFound    = errors.New("user not found")
-	ErrEmailNotFound   = errors.New("email not found")
-	ErrInvalidUserRole = errors.New("invalid user role")
+	ErrUserNotFound       = errors.New("user not found")
+	ErrEmailNotFound      = errors.New("email not found")
+	ErrInvalidUserRole    = errors.New("invalid user role")
+	ErrEmailAlreadyExists = errors.New("email already exists")
 )
