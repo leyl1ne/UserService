4f4e59b607d75b2ef7fa643061e66ae6552e99b1
leyl1ne <d-khodin@inbox.ru>
2026-05-20 07:52:18 +0300

    feat(user):  add UserRole type and ParseUserRole and NewUser func

--- internal/model/user/errors.go
+++ internal/model/user/errors.go
@@ -3,6 +3,7 @@ package user
 import "errors"
 
 var (
-	ErrUserNotFound  = errors.New("user not found")
-	ErrEmailNotFound = errors.New("email not found")
+	ErrUserNotFound    = errors.New("user not found")
+	ErrEmailNotFound   = errors.New("email not found")
+	ErrInvalidUserRole = errors.New("invalid user role")
 )
--- internal/model/user/user.go
+++ internal/model/user/user.go
@@ -1,16 +1,60 @@
 package user
 
 import (
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
 )
 
+type UserRole string
+
+const (
+	Seller    UserRole = "SELLER"
+	Buyer     UserRole = "BUYER"
+	Warehouse UserRole = "WAREHOUSE"
+	Logistics UserRole = "LOGISTICS"
+)
+
+func ParseUserRole(role string) (UserRole, error) {
+	switch UserRole(strings.ToUpper(role)) {
+	case Seller:
+		return Seller, nil
+	case Buyer:
+		return Buyer, nil
+	case Warehouse:
+		return Warehouse, nil
+	case Logistics:
+		return Logistics, nil
+	default:
+		return "", ErrInvalidUserRole
+	}
+
+}
+
+type PasswordHash string
+
 type User struct {
 	ID           uuid.UUID
 	Email        string
 	PasswordHash string
-	Role         string
+	Role         UserRole
 	CompanyID    *uuid.UUID
 	CreatedAt    time.Time
+}
+
+func NewUser(email, passwordHash, role string) (*User, error) {
+
+	userRole, err := ParseUserRole(role)
+	if err != nil {
+		return nil, err
+	}
+
+	return &User{
+		ID:           uuid.New(),
+		Email:        email,
+		PasswordHash: passwordHash,
+		Role:         userRole,
+		CreatedAt:    time.Now(),
+	}, nil
 }
