4fe33dd7f7d071eba93e83592e2df56fcd59dd4f
leyl1ne <d-khodin@inbox.ru>
2026-05-20 11:09:09 +0300

    feat: add helper function for http response

--- internal/transport/http/response/response.go
+++ internal/transport/http/response/response.go
@@ -1,6 +1,14 @@
 package response
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
 
 type Error struct {
 	Message string `json:"message"`
@@ -24,4 +32,58 @@ func WriteErrorAbort(c *gin.Context, status int, msg string) {
 			Message: msg,
 		},
 	})
+}
+
+func WriteInternalServerError(c *gin.Context) {
+	c.JSON(http.StatusInternalServerError, ErrorResponse{
+		Error: Error{
+			Message: "internal server error",
+		},
+	})
+}
+
+func WriteBindError(c *gin.Context, err error) {
+	var ve validator.ValidationErrors
+
+	switch {
+	case errors.Is(err, io.EOF):
+		WriteError(c, http.StatusBadRequest, "request body is empty")
+		return
+	case errors.As(err, &ve):
+		ValidationError(c, ve)
+		return
+	default:
+		WriteError(c, http.StatusBadRequest, "invalid request body")
+		return
+	}
+}
+
+func ValidationError(c *gin.Context, validaitonErr validator.ValidationErrors) {
+	c.JSON(http.StatusUnprocessableEntity, gin.H{
+		"error":  "validation failed",
+		"fields": formatValidationErrors(validaitonErr),
+	})
+}
+
+func formatValidationErrors(ve validator.ValidationErrors) map[string]string {
+	errorsMap := make(map[string]string, len(ve))
+
+	for _, fe := range ve {
+		field := strings.ToLower(fe.Field())
+
+		switch fe.Tag() {
+		case "required":
+			errorsMap[field] = "field is required"
+		case "min":
+			errorsMap[field] = "too short"
+		case "max":
+			errorsMap[field] = "too long"
+		case "email":
+			errorsMap[field] = "invalid email format"
+		default:
+			errorsMap[field] = "invalid value"
+		}
+	}
+
+	return errorsMap
 }
