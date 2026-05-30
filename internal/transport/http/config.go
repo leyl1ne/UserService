56b52a920ef58e8a07f3cba8a42da68f50257dc5
leyl1ne <d-khodin@inbox.ru>
2026-05-30 09:28:47 +0300

    refactor: create BasicAuth and HTTP struct

--- internal/transport/http/config.go
+++ internal/transport/http/config.go
@@ -3,17 +3,21 @@ package http
 import "time"
 
 type Config struct {
-	HTTP struct {
-		Port         int           `yaml:"port"          env:"SERVER_HTTP_PORT"`
-		ReadTimeout  time.Duration `yaml:"read_timeout"  env:"SERVER_HTTP_READ_TIMEOUT"`
-		WriteTimeout time.Duration `yaml:"write_timeout" env:"SERVER_HTTP_WRITE_TIMEOUT"`
-
-		BasicAuth struct {
-			Enabled  bool   `yaml:"enabled"   env:"SERVER_HTTP_BASICAUTH_ENABLED"`
-			Username string `yaml:"username"  env:"SERVER_HTTP_BASICAUTH_USERNAME"`
-			Password string `env:"SERVER_HTTP_BASICAUTH_PASSWORD"`
-		} `yaml:"basic_auth"`
-	} `yaml:"http"`
+	HTTP HTTP `yaml:"http"`
 
 	GracefulShutdown time.Duration `yaml:"graceful_shutdown"  env:"SERVER_GRACEFUL_SHUTDOWN"`
+}
+
+type BasicAuth struct {
+	Enabled  bool   `yaml:"enabled"   env:"SERVER_HTTP_BASICAUTH_ENABLED"`
+	Username string `yaml:"username"  env:"SERVER_HTTP_BASICAUTH_USERNAME"`
+	Password string `env:"SERVER_HTTP_BASICAUTH_PASSWORD"`
+}
+
+type HTTP struct {
+	Port         int           `yaml:"port"          env:"SERVER_HTTP_PORT"`
+	ReadTimeout  time.Duration `yaml:"read_timeout"  env:"SERVER_HTTP_READ_TIMEOUT"`
+	WriteTimeout time.Duration `yaml:"write_timeout" env:"SERVER_HTTP_WRITE_TIMEOUT"`
+
+	BasicAuth BasicAuth `yaml:"basic_auth"`
 }
