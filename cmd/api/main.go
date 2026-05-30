3e5dda116be736990624b297b4a17cc846da1fb1
leyl1ne <d-khodin@inbox.ru>
2026-05-30 10:21:50 +0300

    refactor: add ZerologLogger init func, rename log -> zeroLog

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -16,12 +16,12 @@ import (
 	"github.com/leyl1ne/UserService/internal/logger"
 	"github.com/leyl1ne/UserService/internal/logger/zl"
 	postgresRepo "github.com/leyl1ne/UserService/internal/repository/postgres"
-	httpServer "github.com/leyl1ne/UserService/internal/transport/http"
 	authandler "github.com/leyl1ne/UserService/internal/transport/http/handler/auth"
 	companyhandler "github.com/leyl1ne/UserService/internal/transport/http/handler/company"
 	docshandler "github.com/leyl1ne/UserService/internal/transport/http/handler/docs"
 	healthhandler "github.com/leyl1ne/UserService/internal/transport/http/handler/health"
 	userhandler "github.com/leyl1ne/UserService/internal/transport/http/handler/user"
+	"github.com/leyl1ne/UserService/internal/transport/http/router"
 
 	authservice "github.com/leyl1ne/UserService/internal/service/auth"
 	companyservice "github.com/leyl1ne/UserService/internal/service/company"
@@ -33,10 +33,14 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to load config: %v", err)
 	}
-	log := zl.NewZerologLogger(cfg.Logger.Level, os.Stderr)
 
-	log.Info("http server started!", logger.Field{Key: "Addr", Value: cfg.Server.HTTP.Port})
-	log.Debug("logger debug mode enabled")
+	zeroLog, err := zl.NewZerologLogger(cfg.Logger)
+	if err != nil {
+		log.Fatalf("faieled to init zero logger: %v", err)
+	}
+
+	zeroLog.Info("http server started!", logger.Field{Key: "Addr", Value: cfg.Server.HTTP.Port})
+	zeroLog.Debug("logger debug mode enabled")
 
 	// можно ли передавать notidy context ?
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
@@ -44,14 +48,14 @@ func main() {
 
 	db, err := postgresInfra.Open(ctx, cfg.Postgres)
 	if err != nil {
-		log.Fatal("failed to connect database", logger.Field{Key: "error", Value: err.Error()})
+		zeroLog.Fatal("failed to connect database", logger.Field{Key: "error", Value: err.Error()})
 		os.Exit(1)
 	}
 
 	repo := postgresRepo.NewRepository(db.Pool())
 	passwordHasher, err := password.NewHasher(cfg.PasswordHasher)
 	if err != nil {
-		log.Fatal("failed to create password hasher", logger.Field{Key: "error", Value: err.Error()})
+		zeroLog.Fatal("failed to create password hasher", logger.Field{Key: "error", Value: err.Error()})
 		os.Exit(1)
 	}
 	jwtGenerator := jwt.NewJWTGenerator(cfg.JWTGenerator)
@@ -61,22 +65,22 @@ func main() {
 	userService := userservice.NewService(repo)
 	companyService := companyservice.NewService(repo)
 
-	authHandler := authandler.NewAuthHandler(log, authService)
-	userHandler := userhandler.NewUserHandler(log, userService)
-	companyHandler := companyhandler.NewCompanyHandler(log, companyService)
+	authHandler := authandler.NewAuthHandler(zeroLog, authService)
+	userHandler := userhandler.NewUserHandler(zeroLog, userService)
+	companyHandler := companyhandler.NewCompanyHandler(zeroLog, companyService)
 	healthHandler := healthhandler.NewHealthHandler(cfg.App.Version, cfg.App.Name, cfg.App.Environment)
 	docsHanler, err := docshandler.NewDocsHandler()
 	if err != nil {
-		log.Fatal("failed to create docs handler", logger.Err(err))
+		zeroLog.Fatal("failed to create docs handler", logger.Err(err))
 	}
 
-	router := httpServer.SetupRouter(log, httpServer.Handlers{
+	router := router.SetupRouter(zeroLog, router.Handlers{
 		AuthHandler:    authHandler,
 		UserHandler:    userHandler,
 		CompanyHandler: companyHandler,
 		HealthHandler:  healthHandler,
 		DocsHandler:    docsHanler,
-	}, jwtGenerator)
+	}, jwtGenerator, cfg.Server)
 
 	s := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.Server.HTTP.Port),
@@ -86,7 +90,7 @@ func main() {
 	}
 	go func() {
 		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatal("server failed", logger.Err(err))
+			zeroLog.Fatal("server failed", logger.Err(err))
 		}
 	}()
 
@@ -96,19 +100,19 @@ func main() {
 	defer shutdownCancel()
 
 	if err := s.Shutdown(shutdownCtx); err != nil {
-		log.Error("failed to stop server gracefully", logger.Err(err))
+		zeroLog.Error("failed to stop server gracefully", logger.Err(err))
 
 		if err := s.Close(); err != nil {
-			log.Error("forced shutdown failed", logger.Err(err))
+			zeroLog.Error("forced shutdown failed", logger.Err(err))
 		}
 		return
 
 	}
 
-	log.Info("server exiting")
+	zeroLog.Info("server exiting")
 
 	if err := db.Close(); err != nil {
-		log.Error("failed to close database client", logger.Field{Key: "error", Value: err.Error()})
+		zeroLog.Error("failed to close database client", logger.Field{Key: "error", Value: err.Error()})
 	}
 
 }
